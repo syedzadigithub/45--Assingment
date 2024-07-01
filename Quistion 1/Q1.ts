18e614a2360c6909f3c6698220091620380844b8
@Syeda Huma Jamshed <159490433+syedzadigithub@users.noreply.github.com>
2024-07-01 19:46:46 +0500

    45 Assingment QNo.1
    Q No.1

---
+++ Quistion 1/Q1.ts
@@ -0,0 +1,6 @@
+//starting of typescript journey with the installation of 
+//1.VS Code
+//2. Node JS
+//3.typescript
+
+//Install [Node.js](https://nodejs.org/en/) , [Typescript](https:/www.typescripting.org/download ) and [VS code](https://code.visualstudio.com/) on your computer.
